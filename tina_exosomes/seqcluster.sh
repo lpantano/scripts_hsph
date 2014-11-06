d30599764a268c1dc12fe134b2a6718475bc44e9
Lorena <lorena.pantano@gmail.com>
2014-11-06 18:35:15 -0500

    complete smallrna analysis

--- tina_exosomes/seqcluster.sh
+++ tina_exosomes/seqcluster.sh
@@ -9,13 +9,15 @@ export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 DIR=/n/data1/cores/bcbio/tina_exosomes/work
 PATH=/home/lp113/soft/bcbiome/anaconda/bin:$PATH
+DB=/n/data1/cores/bcbio/tina_exosomes/data
 
 cd $DIR 
 
-while read file cell ; do
- NAME=$cell
- fastq=$file
- seqcluster prepare -c seqcluster.files -o res -l 17 --minc 1 --maxl 50
- STAR 
- seqcluster cluster -m res/seqs.ma -a res/seqs.bam -b $DB/tRNA.bed,$DB/mirna.bed
-done
+seqcluster prepare -c ../seqcluster.files -o res -l 17 --minc 1 --maxl 50
+STAR --genomeDir  /groups/bcbio/bcbio/genomes/Hsapiens/hg19/star --readFilesIn res/seqs.fastq --outFilterMultimapNmax 5000 --outSAMattributes NH HI NM --outSAMtype BAM SortedByCoordinate
+ 
+seqcluster cluster -m res/seqs.ma -a Aligned.sortedByCoord.out.bam -b $DB/tRNA.bed,$DB/miRNA.bed,$DB/refGene.bed,$DB/snoRNA.bed,$DB/rmsk.bed,$DB/wgRNA.bed -o res/cluster
+
+samtools index Aligned.sortedByCoord.out.bam
+
+seqcluster stats -j res/cluster/seqcluster.json -m res/seqs.ma -a Aligned.sortedByCoord.out.bam -o res/stats 
