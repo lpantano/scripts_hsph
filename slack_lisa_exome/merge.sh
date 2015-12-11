915f8420c718c6e36748d2536c8f3c1c873a4992
Lorena <lorena.pantano@gmail.com>
2015-12-11 15:59:01 -0500

    update script to get important variants.

--- slack_lisa_exome/merge.sh
+++ slack_lisa_exome/merge.sh
@@ -1,1 +1,2 @@
 bcftools  merge -f PASS vardict-java/bN*effects.vcf.gz -o vardict-java/samples.vcf.gz -O z
+bcftools query -i 'FILTER==PASS' -f '[\t%AF][\t%DP][\t%SAMPLE]\n' vardict-java/samples.vcf.gz > ../final/2015-12-07_tumor_mm10/samples_af_depth.tsv
--- slack_lisa_exome/prioritize_variants.py
+++ slack_lisa_exome/prioritize_variants.py
@@ -9,6 +9,12 @@ from argparse import ArgumentParser
 def _gtp(info):
     return info.split(":")[0]
 
+def _info_sample(name, info, tag):
+    k = name.split(":")
+    v = info.split(":")
+    d = dict(zip(k, v))
+    return d[tag]
+
 def _is_in_any_control(samples, t, n):
     c = set()
     for s in n:
@@ -32,20 +38,24 @@ def _get_info(info):
             for e in v.split(","):
                 type_e = e.split("(")[0]
                 gene_e = e.split("(")[1].split("|")[5]
-                level_e = e.split("(")[1].split("|")[1]
+                level_e = e.split("(")[1].split("|")[0]
                 effect.append("_".join([type_e, level_e]))
                 genes.append(gene_e)
     return [status, effect, genes]
 
 def _frmt(info, t, n):
+    """Get information for the variant"""
     cols = info.split("\t")
     samples = cols[9:]
     chrom, pos, rs = cols[:3]
     change = "%s:%s" % (cols[3], cols[4])
     status, eff, gene = _get_info(cols[7])
+    effect = ";".join(list(set(["%s=%s" % (e, g) for e, g in zip(eff, gene)])))
     gt_t = ":".join([_gtp(samples[s]) for s in t])
     gt_n = ":".join([_gtp(samples[s]) for s in n])
-    line = ("{chrom}\t{pos}\t{rs}\t{change}\t{status}\t{eff}\t{gene}\t{gt_t}\t{gt_n}")
+    af_t = ":".join([_info_sample(cols[8], samples[s], "AF") for s in t])
+    af_n = ":".join([_info_sample(cols[8], samples[s], "AF") for s in n])
+    line = ("{chrom}\t{pos}\t{rs}\t{change}\t{gt_t}|{gt_n}\t{af_t}|{af_n}\t{status}\t{effect}")
     return line.format(**locals())
 
 def _clean_variants(fn):
@@ -54,21 +64,21 @@ def _clean_variants(fn):
     """
     t = [0, 2, 4]
     n = [1, 3, 5]
+    print "\t".join(["chrom", "pos", "rs", "change", "gt:tumor|normal",
+                     "af:tumor|normal", "status", "effects", "num_samples_seen"])
     with gzip.open(fn[0], 'rb') as vcf_reader:
         for record in vcf_reader:
             skip = 0
             if not record.startswith("#"):
                 samples = record.strip().split("\t")[9:]
-                for s in t + n:
-                    if _gtp(samples[s]) == "./.":
-                        skip = 1
-                if skip == 1:
+                for s in [0, 1, 2]:
+                    skip += _gtp(samples[t[s]]) == "./." or _gtp(samples[n[s]]) == "./."
+                if skip > 1:
                     continue
-                if _is_in_any_control(samples, t, n):
-                    continue
+                # if _is_in_any_control(samples, t, n):
+                #    continue
                 frmt = _frmt(record, t, n)
-                print frmt
-            # control.add("%%%" % (record.CHROM, record.POS, control))
+                print "%s\t%s" % (frmt, 3 - skip)
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Detect enhancer activity.")
